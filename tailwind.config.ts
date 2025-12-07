0e79ca79c3c20d5b02f7490ea61a987ce553411a
doorhandle917-sudo <doorhandle917@gmail.com>
2025-12-07 17:16:50 +0100

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import { skeleton } from '@skeletonlabs/tw-plugin'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'grain': "url(\"data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='2.5' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.35'/%3E%3C/svg%3E\")",
+			},
+			backgroundSize: {
+				'grain': '200px 200px',
+			},
+		},
+	},
+	plugins: [
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+} satisfies Config;
